b77b8ea5518ca717283ce1d2ef980a04dd44dcbf
ic-qialanqian <58182792+ic-qialanqian@users.noreply.github.com>
2022-12-08 17:18:15 +0000

    Update generate_salmap.py

--- generate_salmap.py
+++ generate_salmap.py
@@ -7,16 +7,16 @@ from torch.autograd import Variable
 from torchvision import transforms
 from config import test_data
 from misc import check_mkdir, crf_refine
-from DANet_Ours import RGBD_sal
+from Mask-Guided-Net import RGBD_sal
 import argparse
 import cv2
 import torch.nn.functional as functional
-torch.manual_seed(2018)
+torch.manual_seed(2022)
 torch.cuda.set_device(0)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--ckpt_path', type=str, default='./model')
-parser.add_argument('--exp_name', type=str, default='2VGG19_baseline_denseprogessfilter_5rgbfilter_deformable(SOTA)')
+parser.add_argument('--exp_name', type=str, default='')
 parser.add_argument('--snapshot', type=str, default='20500')
 parser.add_argument('--crf_refine', type=bool, default=False)
 parser.add_argument('--save_results', type=bool, default=True)
@@ -45,19 +45,9 @@ def main(args):
             img_list = [os.path.splitext(f)[0] for f in os.listdir(root1) if f.endswith('.png')]
             for idx, img_name in enumerate(img_list):
                 print ('predicting for %s: %d / %d' % (name, idx + 1, len(img_list)))
-                img1 = Image.open(os.path.join(root,'RGB',img_name + '.jpg')).convert('RGB')
-                #img1 = Image.open(os.path.join(root,'image',img_name + '.jpg')).convert('RGB')
+                img1 = Image.open(os.path.join(root,'RGB',img_name + '.jpg')).convert('RGB')                
                 depth = Image.open(os.path.join(root,'depth',img_name + '.png')).convert('L')
-                gt = Image.open(os.path.join(root,'GT',img_name + '.png'))
-
-                '''
-                for y in range(depth.height):
-                    for x in range(depth.width):
-                        value = depth.getpixel((x, y))
-                        #print(value)
-                        new_color = 255 -value
-                        depth.putpixel((x, y), new_color) 
-                '''
+                gt = Image.open(os.path.join(root,'GT',img_name + '.png')
                 
                 img = img1
                 w_,h_ = img1.size
@@ -67,19 +57,12 @@ def main(args):
                 img_var = Variable(img_transform(img1).unsqueeze(0), volatile=True).cuda()
                 depth = Variable(depth_transform(depth).unsqueeze(0), volatile=True).cuda()
                 gt = Variable(depth_transform(gt).unsqueeze(0), volatile=True).cuda()
-                #prediction = net(img_var)
-                prediction,mask = net(img_var,depth)
-                #prediction = F.softmax(prediction,dim=1)
-                #prediction = prediction[0][1]
-                #prediction = to_pil(prediction.data.squeeze(0).cpu())
-                #prediction = prediction.resize((w_, h_), Image.BILINEAR)
-                #d = torch.sigmoid(d)
-                #masked_d = torch.sigmoid(mask)
-                
-                #prediction= to_pil(prediction.data.squeeze(0).cpu())
-                #prediction = prediction.resize((w_, h_), Image.BILINEAR)
-                masked_d = to_pil(mask.data.squeeze(0).cpu())
-                prediction = masked_d.resize((w_, h_), Image.BILINEAR)
+               
+                prediction = net(img_var,depth)
+               
+                prediction = to_pil(prediction.data.squeeze(0).cpu())
+                prediction = prediction.resize((w_, h_), Image.BILINEAR)
+           
                 
                 
                 if args.crf_refine:
